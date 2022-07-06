7dca9b81d86de403c0206c094aa6e7db1554fbd9
suck5u <hugo.suck4@etu.univ-lorraine.fr>
2022-07-06 14:38:22 +0200

    Suppression de l'import "App.css"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Contact from './components/Contact';
 import DontRollBack from './components/DontRollBack';
 import Home from './components/Home';
