19a644b643f4f3c7dddcb8f730c472a1d072359d
suck5u <hugo.suck4@etu.univ-lorraine.fr>
2022-07-07 17:48:05 +0200

    Refonte des css + background dino

--- src/components/Bloc.js
+++ src/components/Bloc.js
@@ -4,27 +4,34 @@ const Bloc = (props) => {
     if (props.isImgFirst) {
         return (
             <div className='bloc'>
-                <div>
-                    {/*<img src='./Logo_JurassicIslas.png' alt='Logo Ju&Go' />*/}
-                    <img className='image' src={props.imgSrc} alt='Logo Ju&Go' />
-                </div>
-                <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, consectetur quia, deleniti molestiae pariatur, officiis id incidunt unde illo porro accusantium! Eaque corrupti, atque quas molestias placeat velit nemo debitis? Dolorum accusantium quaerat iusto quis id porro adipisci qui voluptatem quibusdam? In, quasi nobis odit harum nulla dolore enim saepe, distinctio libero corporis molestias, omnis asperiores quas ex officia. Dolor reprehenderit autem laborum dolore quasi unde, consectetur impedit corporis, itaque obcaecati vitae architecto voluptate iure corrupti harum? Praesentium, itaque beatae? Veritatis, aperiam laborum vel sint vero veniam necessitatibus debitis quae itaque, repudiandae corporis voluptas hic commodi animi totam quo dolorem obcaecati modi dignissimos? Molestias ducimus voluptate eveniet iure atque numquam quia doloribus, vero vitae repellendus distinctio recusandae iste laboriosam quas commodi autem voluptates quidem ratione molestiae id aliquam quos assumenda praesentium? Architecto id minima ullam aliquam mollitia tempora aliquid ad hic laudantium? Qui accusantium quidem mollitia dolorem libero? Aut laudantium sunt dolorum nobis veritatis ullam placeat fugiat tenetur, eligendi ipsam cum ea tempore, ratione unde saepe voluptatum atque eaque quasi voluptates laborum in molestias blanditiis fuga tempora. Omnis, reiciendis nostrum quod iusto asperiores dolorem explicabo consequuntur similique, sed suscipit blanditiis exercitationem temporibus error unde quasi ut voluptatum facilis quidem cum?</p>
-                </div>
+                <Img imgSrc={props.imgSrc} />
+                <Text />
             </div>
         );
     } else {
         return (
             <div className='bloc'>
-                <div>
-                    <p className='textBloc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, consectetur quia, deleniti molestiae pariatur, officiis id incidunt unde illo porro accusantium! Eaque corrupti, atque quas molestias placeat velit nemo debitis? Dolorum accusantium quaerat iusto quis id porro adipisci qui voluptatem quibusdam? In, quasi nobis odit harum nulla dolore enim saepe, distinctio libero corporis molestias, omnis asperiores quas ex officia. Dolor reprehenderit autem laborum dolore quasi unde, consectetur impedit corporis, itaque obcaecati vitae architecto voluptate iure corrupti harum? Praesentium, itaque beatae? Veritatis, aperiam laborum vel sint vero veniam necessitatibus debitis quae itaque, repudiandae corporis voluptas hic commodi animi totam quo dolorem obcaecati modi dignissimos? Molestias ducimus voluptate eveniet iure atque numquam quia doloribus, vero vitae repellendus distinctio recusandae iste laboriosam quas commodi autem voluptates quidem ratione molestiae id aliquam quos assumenda praesentium? Architecto id minima ullam aliquam mollitia tempora aliquid ad hic laudantium? Qui accusantium quidem mollitia dolorem libero? Aut laudantium sunt dolorum nobis veritatis ullam placeat fugiat tenetur, eligendi ipsam cum ea tempore, ratione unde saepe voluptatum atque eaque quasi voluptates laborum in molestias blanditiis fuga tempora. Omnis, reiciendis nostrum quod iusto asperiores dolorem explicabo consequuntur similique, sed suscipit blanditiis exercitationem temporibus error unde quasi ut voluptatum facilis quidem cum?</p>
-                </div>
-                <div>
-                    <img src={props.imgSrc} alt='Logo Ju&Go' />
-                </div>
+                <Text />
+                <Img imgSrc={props.imgSrc} />
             </div>
         );
     }
 };
+
+const Text = () => {
+    return (
+        <div>
+            <p className='textBloc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, consectetur quia, deleniti molestiae pariatur, officiis id incidunt unde illo porro accusantium! Eaque corrupti, atque quas molestias placeat velit nemo debitis? Dolorum accusantium quaerat iusto quis id porro adipisci qui voluptatem quibusdam? In, quasi nobis odit harum nulla dolore enim saepe, distinctio libero corporis molestias, omnis asperiores quas ex officia. Dolor reprehenderit autem laborum dolore quasi unde, consectetur impedit corporis, itaque obcaecati vitae architecto voluptate iure corrupti harum? Praesentium, itaque beatae? Veritatis, aperiam laborum vel sint vero veniam necessitatibus debitis quae itaque, repudiandae corporis voluptas hic commodi animi totam quo dolorem obcaecati modi dignissimos? Molestias ducimus voluptate eveniet iure atque numquam quia doloribus, vero vitae repellendus distinctio recusandae iste laboriosam quas commodi autem voluptates quidem ratione molestiae id aliquam quos assumenda praesentium? Architecto id minima ullam aliquam mollitia tempora aliquid ad hic laudantium? Qui accusantium quidem mollitia dolorem libero? Aut laudantium sunt dolorum nobis veritatis ullam placeat fugiat tenetur, eligendi ipsam cum ea tempore, ratione unde saepe voluptatum atque eaque quasi voluptates laborum in molestias blanditiis fuga tempora. Omnis, reiciendis nostrum quod iusto asperiores dolorem explicabo consequuntur similique, sed suscipit blanditiis exercitationem temporibus error unde quasi ut voluptatum facilis quidem cum?</p>
+        </div>
+    )
+}
+
+const Img = (props) => {
+    return (
+        <div>
+            <img className='image' src={props.imgSrc} alt='Logo Ju&Go' />
+        </div>
+    )
+}
 
 export default Bloc;
--- src/components/JurassicIslas.js
+++ src/components/JurassicIslas.js
@@ -6,11 +6,12 @@ import Navigation from './Navigation';
 
 const JurassicIslas = () => {
     return (
-        <div>
+        <div className='backgroundImg'>
             <Helmet>
                 <title>Ju&Go | Jurassic Islas</title>
             </Helmet>
             <Navigation />
+            <div></div>
             <h1>Jurassic Islas</h1>
             <Bloc imgSrc="./Logo_JurassicIslas.png" isImgFirst={true} />
             <Bloc imgSrc="./Logo_JueGo.png" />
