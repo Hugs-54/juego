36f64c64db343c94302dd5fa07a51c6ce515e194
Hugs <hugobob44@gmail.com>
2025-06-07 12:51:42 +0200

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
 import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
     window.location.replace("https://juego-boardgames.vercel.app");
   }, []);
-
   return null;
-  {/*return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-         
-        <Route path="/jurassic-islas" element={<JurassicIslas />} />
-        <Route path="/dont-roll-back" element={<DontRollBack />} />
-        <Route path="/ultime-victime" element={<UltimeVictime />} />
-        <Route path="/monster-snacks" element={<MonsterSnacks />} />
-        <Route path="/contact" element={<Contact />} />
-
-        {/*path="*" va vers l'accueil si l'url n'est pas reconnu
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
-  */}
 }
 
 export default App;
